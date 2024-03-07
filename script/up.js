12df36a36a4411ba8b3d90bebf7b6796ad84aaae
Maxim Zavidiy <zavidiy1998@mail.ru>
2024-03-07 20:47:56 +0200

    first_commit

---
+++ script/up.js
@@ -0,0 +1,10 @@
+/* Для всплывающего окна */ 
+let menuBtn = document.querySelector('.menuBtn');
+let menu = document.querySelector('nav');
+let link = document.querySelector('.link');
+menuBtn.addEventListener('click', () =>{
+menuBtn.classList.toggle('active');
+menu.classList.toggle('popUp');
+link.classList.remove('active');
+});
+
