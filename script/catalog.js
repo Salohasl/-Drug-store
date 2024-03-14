ec786de4381322f93dc1967338b2d02a51eebae4
Maxim Zavidiy <zavidiy1998@mail.ru>
2024-03-14 19:52:35 +0200

    v3

--- script/catalog.js
+++ script/catalog.js
@@ -2,11 +2,6 @@ function filterFunc(){
     let filterBtn = document.querySelectorAll('.filterBtn');
     let product = document.querySelectorAll('.description-product');
 
-    let res = [];
-    product.forEach(elem => {
-        res.push(elem.dataset.name);
-    })
-    console.log(res)
 
     filterBtn.forEach(item =>{
         item.addEventListener('click', ()=>{
---
+++ script/img.js
@@ -0,0 +1,25 @@
+function filterFunc(){
+    let filterBtn = document.querySelectorAll('.choiceBtn');
+    let product = document.querySelectorAll('.imgblock');
+    let price = document.querySelector('.price');
+
+    filterBtn.forEach(item =>{
+        item.addEventListener('click', ()=>{
+            for(let el of product){
+                if(item.value == el.dataset.name){
+                    el.style.display = 'block';
+                    price.textContent = item.dataset.price;
+
+                }else{
+                    el.style.display = 'none';
+                }
+            }
+            for(let it of filterBtn){
+                if(it != this){
+                    it.classList.remove('active');
+                }
+            }
+            item.classList.toggle('active');
+        })
+    })
+}filterFunc();
